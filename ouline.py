55427ba58c7c8ac1dd3aea6a7c37682256181a3e
Armel Tchomnou <89040665+Rayarmel@users.noreply.github.com>
2022-04-04 16:20:11 -0400

    My first change

--- ouline.py
+++ ouline.py
@@ -1,10 +1,15 @@
 #import beautifulsoup and request here
+import bs4 as bs
+import urllib.request
 
 
 #function to get job list from url 'https://www.indeed.com/jobs?q={role}&l={location}'
 def getJobList():
     url = 'https://www.indeed.com/jobs?q={role}&l={location}'
     # Complete the missing part of this function here 
+    source = urllib.request.urlopen(url).read()
+    soup = bs.BeautifulSoup(source,'lxml')
+    print(soup.get_text())
 
 #save data in csv file
 def saveDataInCSV():
